e11babdaf3db5444eebeee10772e58a40154f233
APIVerve Team <hello@apiverve.com>
2025-03-28 17:53:35 -0500

    Bug Fixes

---
+++ src/apiverve_sudokugenerator/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_sudokugenerator',
+    version='1.1.9',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Sudoku is a simple tool for generating Sudoku puzzles. It returns a Sudoku puzzle.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
